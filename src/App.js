0693b4c19ac594fa5e9f80ba35543a63dcecded4
tatidan <t.danyliuk@gmail.com>
2021-07-26 02:54:42 +0300

    modal logics added

--- src/App.js
+++ src/App.js
@@ -1,17 +1,28 @@
 import React, { Component } from "react";
 import ImageGallery from "./components/imageGallery/ImageGallery";
+import Modal from "./components/modal/Modal";
 import Searchbar from "./components/searchbar/Searchbar";
 
 class App extends Component {
-  state = {};
+  state = {
+    showModal: false,
+  };
+
+  toggleModal = () => {
+    this.setState(({ showModal }) => ({
+      showModal: !showModal,
+    }));
+  };
 
   onSubmit = (e) => {
     console.log(e);
   };
 
   render() {
+    const { showModal } = this.state;
     return (
       <div className="App">
+        {showModal && <Modal onClose={this.toggleModal} />}
         <Searchbar onSubmit={this.onSubmit} />
         <ImageGallery />
       </div>
--- src/components/modal/Modal.js
+++ src/components/modal/Modal.js
@@ -1,23 +1,48 @@
-//При клике по элементу галереи должно открываться модальное окно с темным оверлеем 
-//и отображаться большая версия изображения.Модальное окно должно закрываться 
+//При клике по элементу галереи должно открываться модальное окно с темным оверлеем
+//и отображаться большая версия изображения.Модальное окно должно закрываться
 //по нажатию клавиши ESC или по клику на оверлее.
 
-//Внешний вид похож на функционал этого VanillaJS-плагина, только вместо 
+//Внешний вид похож на функционал этого VanillaJS-плагина, только вместо
 //белого модального окна рендерится изображение(в примере нажми Run).
 //Анимацию делать не нужно!
 
-import React from 'react';
+import React, { Component } from "react";
+import { createPortal } from "react-dom";
 
-const Modal = () => {
-  return (
-    <div className="Overlay">
-      <div className="Modal">
-        <img src="" alt="" />
-      </div>
-    </div>
-  )
-};
+const modalRoot = document.querySelector("#modal-root");
 
-export default Modal;
+export default class Modal extends Component {
+  componentDidMount() {
+    window.addEventListener("keydown", this.handleKeyDown);
+  }
 
+  componentWillUnmount() {
+    console.log("will unmount");
+    window.removeEventListener("keydown", this.handleKeyDown);
+  }
 
+  handleKeyDown = (e) => {
+    if (e.code === "escape") {
+      console.log("ESC, close modal");
+      this.props.onClose();
+    }
+  };
+  handleBackdropClick = (e) => {
+    console.log("click onto backdrop");
+    if (e.currentTarget === e.target) {
+      this.props.onClose();
+    }
+  };
+  render() {
+    return createPortal(
+      <div className="Overlay" onClick={this.handleBackdropClick}>
+        <div className="Modal">
+          <img src="" alt="" />
+        </div>
+      </div>,
+      modalRoot
+    );
+  }
+}
+
+// {/* {this.props.children} */}
